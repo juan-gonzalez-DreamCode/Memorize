f1b429c02e3657bd5ef380e95030fa6a3ef52864
Juan Gonzalez <juangon.t@gmail.com>
2024-02-05 17:15:03 -0500

    Init Commit Test

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by DreamCode on 5/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
