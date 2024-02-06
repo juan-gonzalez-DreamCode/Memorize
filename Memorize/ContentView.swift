452a82ed06efba7ed1e1df9d128612c880a62305
Juan Gonzalez <juangon.t@gmail.com>
2024-02-06 09:00:38 -0500

    CardView isFaceUp Toggle

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Juan!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.red)
         .padding()
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = false
+    
+    var body: some View {
+         ZStack{
+            let base = RoundedRectangle(cornerRadius: 12.0)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder( lineWidth: 2 )
+                Text("👻")
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
